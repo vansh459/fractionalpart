ae4b42b6ca01d810a32025f409c296d41c362368
vansh459 <68744644+vansh459@users.noreply.github.com>
2023-09-25 14:35:24 +0530

    Create fractionalpart.c

---
+++ fractionalpart.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float x;
+    printf("enter your number=");
+    scanf("%f",&x);
+    
+    int y=x;
+    
+    float z =x - y;
+    printf("%f",z);
+    return 0;
+}
